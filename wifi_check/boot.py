6ae64b588fbc37e5f3056beca977cbc1227eff0d
alex <alex_is_ok@yahoo.com>
2019-05-30 23:06:02 +0300

    button added

--- wifi_check/boot.py
+++ wifi_check/boot.py
@@ -1,20 +1,24 @@
 import network
 import machine
+import json
 
 red = machine.Pin(5, machine.Pin.OUT)
 yellow = machine.Pin(4, machine.Pin.OUT)
 green = machine.Pin(0, machine.Pin.OUT)
+button = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_UP)
 
-wifi = 'WIFI ESSID'
-pwd = 'WIFI PASS'
+with open('data.txt') as json_file:
+    keys = json.load(json_file)
+    essid = keys['essid']
+    pwd = keys['pwd']
+    print('Status OK...')
 
 
 def do_connect(essid, password):
-    red.on()
+    yellow.on()
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
-        yellow.on()
         sta_if.active(True)
         sta_if.connect(essid, password)
         while not sta_if.isconnected():
@@ -23,6 +27,13 @@ def do_connect(essid, password):
     green.on()
 
 
-do_connect(wifi, pwd)
+while True:
+    red.on()
+    if not button.value():
+        print('Button pressed!\nCheck wifi...')
+        do_connect(essid, pwd)
+        red.off()
+        break
+
 params = network.WLAN(network.STA_IF).ifconfig()
 print('network config:', params)
